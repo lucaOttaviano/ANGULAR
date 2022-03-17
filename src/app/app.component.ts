f62fd6df091946e7a0186cc41a0dbd9480af8c98
lucaOttaviano <luca.ottaviano.lavoro@gmail.com>
2022-03-17 11:43:08 +0100

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {  
   input: string = ""; //inizializza la variabile input che corrisponde all'input di testo
   names: string[] = []; // inizializzo l'array in cui si andranno ad inserire gli input
-  counter: number = 0;
+  counter: number = 0; // inizializzo il counter a 0
   testF(){
       this.names[this.counter] = this.input; // inserisci nel names[counter] il valore dell'input di testo
       this.counter++; // incrementa il counter
+      let id = this.counter;
+  }
+  verify: boolean = false;
+  remove(){
+    // this.verify = !this.verify;
+    this.names.splice((this.id), 1);
+    console.log(this.i)
   }
 }
