56873ae33e5523f99c70b5236054932613694029
lucaOttaviano <luca.ottaviano.lavoro@gmail.com>
2022-03-17 09:01:05 +0100

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,12 @@ import { users } from '../app/mock/array';
 })
 export class AppComponent {  
   users: any = users;
-  vari: string[] = [];
-
-  names: any[] = [
-    {"id": 'Mario'},
-    {"id": 'Luca'},
-    {"id": 'Giovanna'},
-    {"id": 'Paola'}
-]
- 
+  input: string = ""; //inizializza la variabile input che corrisponde all'input di testo
+  names: string[] = []; // inizializzo l'array in cui si andranno ad inserire gli input
+  counter: number = 0;
+  testF(){
+      this.names[this.counter] = this.input; // inserisci nel names[counter] il valore dell'input di testo
+      this.counter++; // incrementa il counter
+  }
+  
 }
