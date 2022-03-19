5c14f59db2a316e8373f72d1a425680d400d1441
lucaOttaviano <luca.ottaviano.lavoro@gmail.com>
2022-03-19 12:40:44 +0100

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,17 @@ export class AppComponent {
   input: string = ""; //inizializza la variabile input che corrisponde all'input di testo
   names: string[] = []; // inizializzo l'array in cui si andranno ad inserire gli input
   counter: number = 0; // inizializzo il counter a 0
-  id: number = 0;
-  testF(){ // inserisce gli input di testo nell'array
-      this.names[this.counter] = this.input; // inserisci nel names[counter] il valore dell'input di testo
-      this.id = this.counter;
-      this.counter++; // incrementa il counter
-      console.log(this.id+1)
-      this.test = false;
-  }
   test: boolean = false;
+  testF(){ // inserisce gli input di testo nell'array
+      this.names[this.counter] = this.input; // inserisci nel in ogni elemento di names il valore dell'input di testo
+      this.counter++; // incrementa il counter
+  }
   remove(){
-    // this.names.splice(this.id, 1);
     // console.log(this.names[this.id]);
-    this.test = true;
+    for(let i = 1; i < this.names.length; i++){
+      this.names.splice(i, 1);
+      console.log(i)
+    }
+      
   }
 }
