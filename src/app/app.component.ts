eccb433a8e4b0887b86cf701e03260b5061aa5fa
lucaOttaviano <luca.ottaviano.lavoro@gmail.com>
2022-03-28 09:02:43 +0200

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   input: string = ""; //inizializza la variabile input che corrisponde all'input di testo
-  names: string[] = []; // inizializzo l'array in cui si andranno ad inserire gli input
+  names: Interfaccia[] = []; // inizializzo l'array in cui si andranno ad inserire gli input
   counter: number = 0; // inizializzo il counter a 0
+  id: number = 0; 
   test: boolean = false;
   testF(){ // inserisce gli input di testo nell'array
-      this.names[this.counter] = this.input; // inserisci nel in ogni elemento di names il valore dell'input di testo
-      this.counter++; // incrementa il counter
+      this.id = this.names.length; 
+      this.names.push({id: this.id, name: this.input});
   }
-  remove(name: string){
-    this.names = this.names.filter(x=>x!=name);
+  remove(item: Interfaccia){
+    this.names = this.names.filter(x => x.id != item.id);
   }
+}
+
+interface Interfaccia {
+  id: number;
+  name: string; 
 }
 
 
